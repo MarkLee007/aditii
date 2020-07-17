487a1f4884c9a8bb021f271b5e808f6f91ece1be
MarkLee007 <67710668+MarkLee007@users.noreply.github.com>
2020-07-17 22:05:51 +0300

    Add files via upload

---
+++ javascript/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider__body').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 2000,
+	  slidesToShow: 1,
+	  adaptiveHeight: true,
+	  arrows: false,
+	  autoplay: true,
+  		autoplaySpeed: 3000,
+  });
+});
